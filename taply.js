3329f5a0f80274f5b35bd17af244356865a678c3
Jerry <jones.jeremydavid@gmail.com>
2015-11-05 22:06:22 -0600

    archiving and unarchiving taplists

--- taply.js
+++ taply.js
@@ -113,13 +113,61 @@ if (Meteor.isClient) {
 
     Template.tapLists.helpers({
         'tapList': function(){
-            return TapLists.find({}, {sort: {name: 1}});
+            return TapLists.find({archived: false}, {sort: {name: 1}});
         },
 
-        'tapListCount': function() {
-            return TapLists.find().count();
+    });
+
+    Template.archivedTapLists.helpers({
+        'archivedTapList': function(){
+            return TapLists.find({archived: true}, {sort: {name: 1}});
+        },
+    });
+
+    Template.tapLists.events({
+        'click .delete-taplist': function(e) {
+            e.preventDefault();
+            var tapListId = this._id;
+
+            // TODO: Give some kind of warning message
+
+            Meteor.call("deleteTapList", tapListId, function(error, results) {
+                if(error) {
+                    console.log(error.reason);
+                } else {
+                    // success!
+                    console.log('Deleted '+tapListId);
+                }
+            });
         },
 
+        'click .archive-taplist': function(e) {
+            e.preventDefault();
+            var tapListId = this._id;
+
+            Meteor.call("archiveTapList", tapListId, function(error, results) {
+                if(error) {
+                    console.log(error.reason);
+                } else {
+                    // success!
+                    console.log('Archived '+tapListId);
+                }
+            });
+        },
+
+        'click .unarchive-taplist': function(e) {
+            e.preventDefault();
+            var tapListId = this._id;
+
+            Meteor.call("unarchiveTapList", tapListId, function(error, results) {
+                if(error) {
+                    console.log(error.reason);
+                } else {
+                    // success!
+                    console.log('UnArchived '+tapListId);
+                }
+            });
+        },
     });
 
     Template.addTapList.events({
@@ -143,6 +191,27 @@ if (Meteor.isClient) {
             $('[name=name]').val('');
         }
 
+    });
+
+    Template.addTapForm.events({
+        'submit .add-tap': function(e) {
+            e.preventDefault();
+            var beerName = $('#beer-name').val();
+            var tapListId = $('#taplist-id').val();
+
+            // insert a tap into the collection
+            Meteor.call("addNewTap", beerName, tapListId, function(error, results) {
+                if(error) {
+                    console.log(error.reason);
+                } else {
+                    // success! Add the tap to the taplist
+                    var id = results; // returns the id of the tap created
+                    // clear the input
+                    $('#beer-name').val('');
+                }
+            });
+
+        },
     });
 
     Template.taps.helpers({
@@ -219,49 +288,6 @@ if (Meteor.isClient) {
         return isOwner;
     });
 
-
-
-    Template.tapListPage.events({
-        'submit .add-tap': function(e) {
-            e.preventDefault();
-            var beerName = $('#beer-name').val();
-            var tapListId = $('#taplist-id').val();
-
-            // insert a tap into the collection
-            Meteor.call("addNewTap", beerName, tapListId, function(error, results) {
-                if(error) {
-                    console.log(error.reason);
-                } else {
-                    // success! Add the tap to the taplist
-                    var id = results; // returns the id of the tap created
-                    // clear the input
-                    $('#beer-name').val('');
-                }
-            });
-
-        },
-
-    });
-
-
-    Template.tapLists.events({
-        'click .delete-taplist': function(e) {
-            e.preventDefault();
-            var tapListId = this._id;
-
-            // TODO: Give some kind of warning message
-
-            Meteor.call("deleteTapList", tapListId, function(error, results) {
-                if(error) {
-                    console.log(error.reason);
-                } else {
-                    // success!
-                    console.log('Deleted '+tapListId);
-                }
-            });
-        }
-    });
-
 }
 
 if (Meteor.isServer) {
@@ -314,6 +340,22 @@ if (Meteor.isServer) {
             // delete the taplist
             TapLists.remove(tapListId);
 
+        },
+
+        archiveTapList: function(tapListId) {
+            isTapListOwner(Meteor.userId(), tapListId);
+
+            TapLists.update(tapListId, {
+                $set : {archived: true}
+            });
+        },
+
+        unarchiveTapList: function(tapListId) {
+            isTapListOwner(Meteor.userId(), tapListId);
+
+            TapLists.update(tapListId, {
+                $set : {archived: false}
+            });
         },
 
         addNewTap: function(beerName, parentID) {
