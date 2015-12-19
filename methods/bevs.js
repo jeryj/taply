f22946156c2a4173d9265ba916b098a463c41887
Jerry <jones.jeremydavid@gmail.com>
2015-12-19 10:50:28 -0600

    set onTap to false for taps that have a deleted bev

--- methods/bevs.js
+++ methods/bevs.js
@@ -6,7 +6,7 @@ Meteor.methods({
         // check to make sure the value is a string
         check(bev.name, String);
 
-        if(bev.name == "") {
+        if(bev.name === "") {
             throw new Meteor.Error("no-bev-name", "Yo! Enter a name for your bev.");
         }
 
@@ -18,7 +18,7 @@ Meteor.methods({
                     createdAt: new Date(),
                     owner: Meteor.user().username,
                     ownerId: Meteor.userId(),
-                    }
+                };
 
         var newBev = Bevs.insert(data);
         return Bevs.findOne({_id: newBev});
@@ -29,6 +29,19 @@ Meteor.methods({
         // check permissions
         Meteor.call("isBevOwner", Meteor.userId(), bevId);
 
+        var removeBevFromTap = [];
+        // If it's on tap anywhere, remove it from being on tap
+        Taps.find({onTap: bevId}, {fields: {_id: 1}}).forEach(function(obj) {
+            removeBevFromTap.push(obj._id);
+        });
+
+        // if we have any on tap, set them to false ontap
+        if(removeBevFromTap.length) {
+            Taps.update({_id: {$in: removeBevFromTap}},
+                {$set : {onTap: false}},
+                {multi: true} // allows multiple to be updated
+            );
+        }
         // they own it, so... it's gone!
         // TODO: Soft delete, probably.
         Bevs.remove(bevId);
