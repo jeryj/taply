609f07d156fcea9444219ad2b55182d351a9b6a3
Jerry <jones.jeremydavid@gmail.com>
2016-04-17 10:25:40 -0500

    close button for drawers

--- client/js/display.js
+++ client/js/display.js
@@ -9,9 +9,11 @@ Template.display.helpers({
 Template.display.events({
     'click .display__name a': function(e) {
         e.preventDefault();
-        console.log(this._id);
-        console.log($('#bev--'+this._id));
         $('.bev-details__bev').removeClass('bev-details__bev--active');
         $('#bev--'+this._id).addClass('bev-details__bev--active');
-    }
+    },
+    'click .bev-details__close': function(e) {
+        e.preventDefault();
+        $('#bev--'+this._id).removeClass('bev-details__bev--active');
+    },
 });
